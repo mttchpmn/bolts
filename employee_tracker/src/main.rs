97de9180e8fc224b642dd4ef393dfb42522b0d49
Matt Chapman <matt@mattchapman.io>
2022-01-25 09:32:54 +1300

    Add employee tracker

---
+++ employee_tracker/src/main.rs
@@ -0,0 +1,47 @@
+use std::collections::HashMap;
+
+fn main() {
+    println!("-----| EMPLOYEE TRACKER |-----");
+    let mut dm = DepartmentMap::new();
+
+    dm.add_to_department("engineering", "Alex Henderson");
+    dm.add_to_department("engineering", "Matt Chapman");
+
+    dm.add_to_department("product", "Leonie Wise");
+
+    dm.list_all_departments();
+}
+
+// fn get_user_command() {}
+
+#[derive(Debug)]
+struct DepartmentMap {
+    map: HashMap<String, Vec<String>>,
+}
+
+impl DepartmentMap {
+    fn new() -> DepartmentMap {
+        DepartmentMap {
+            map: HashMap::new(),
+        }
+    }
+
+    fn add_to_department(&mut self, department_name: &str, employee: &str) {
+        let department = self.map.get_mut(department_name);
+
+        match department {
+            Some(department) => department.push(String::from(employee)),
+            None => {
+                self.map
+                    .insert(String::from(department_name), vec![String::from(employee)]);
+            }
+        }
+    }
+
+    fn list_all_departments(&self) {
+        for (key, value) in &self.map {
+            println!("Department: {}", key);
+            println!("Employees: {}", value.join(", "));
+        }
+    }
+}
